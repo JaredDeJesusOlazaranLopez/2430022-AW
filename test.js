da61a489cc883aedb8af5b2b61e5a9cc538442f7
Jared de Jesus Olazaran Lopez <2430022@upv.edu.mx>
2025-09-24 12:46:20 -0600

    Subi un test js

---
+++ test.js
@@ -0,0 +1,22 @@
+console.log("Hola mundo");
+console.log("Adiós mundo");
+
+var x=5;
+var y=10;
+var z=x*y;
+
+console.log(z);
+
+console.log(x.toString()+2+2);
+
+if(typeof x == "number"){
+    console.log("x es un número");
+}else{
+    console.log("x no es un número");
+}
+
+if(x!="5"){
+    console.log("si");
+} else{
+    console.log("no");
+}
