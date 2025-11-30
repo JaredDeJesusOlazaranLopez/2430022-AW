3115a38d085395392d387ab78dcc0d1b9a019649
Jared <2430022@upv.edu.mx>
2025-11-30 09:14:26 -0600

    Sidebar

--- Practica-9/Pagos/Pagos.php
+++ Practica-9/Pagos/Pagos.php
@@ -12,7 +12,7 @@ $nivelCarpeta = '../';
     <title>Dashboard</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
-    <link rel="stylesheet" href="styles/stylesD.css">
+    <link rel="stylesheet" href="../styles/stylesD.css">
 </head>
 
 <body class="overflow-x-hidden">
